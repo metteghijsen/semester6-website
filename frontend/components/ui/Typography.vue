1d2c23711c02178220a2f88a7a309f2c811a56c0
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-18 19:02:15 +0200

    Feature: Started working on typography component

---
+++ frontend/components/ui/Typography.vue
@@ -0,0 +1,63 @@
+<template>
+  <Component :is="type" :class="`${size} ${color}`">
+    <slot />
+  </Component>
+</template>
+
+<script>
+export default defineComponent({
+  name: 'Typography',
+  props: {
+    type: {
+      default: 'p',
+      required: false,
+      type: String
+    },
+    size: {
+      default: 'text',
+      required: false,
+      type: String
+    },
+    color: {
+      default: 'text-black100',
+      required: false,
+      type: String
+    }
+  }
+})
+</script>
+
+    <style>
+      .font-primary{
+        @apply font-poppins font-bold
+      }
+
+      .font-button{
+        @apply font-montserrat font-bold uppercase
+      }
+
+      /* Headings */
+
+      .heading1{
+        @apply font-primary md:text-[60px] text-[46px] leading-[64px]
+      }
+
+      .heading2{
+        @apply font-primary md:text-[40px] text-[30px] leading-[44px]
+      }
+
+      .heading3{
+        @apply font-montserrat font-bold md:text-[34px] text-[26px] leading-[38px]
+      }
+
+      /* Button fonts */
+
+      .buttonbig{
+        @apply font-button text-[24px] leading-[24px]
+      }
+
+      .buttonregular{
+        @apply font-button text-[20px] leading-[24px]
+      }
+
+    </style>
