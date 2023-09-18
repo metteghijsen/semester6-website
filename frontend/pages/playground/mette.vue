a9f5d892ebb5fd69fae0dd1866ac3c528f0aaad7
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-18 18:19:50 +0200

    Feature: Added playground for testing purposes

---
+++ frontend/pages/playground/mette.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="h-full overflow-x-auto">
+    <div class="bg-white100 my-2">
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-red100" />
+        <div class="w-3 h-3 m-1 bg-red60" />
+        <div class="w-3 h-3 m-1 bg-red40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-orange100" />
+        <div class="w-3 h-3 m-1 bg-orange60" />
+        <div class="w-3 h-3 m-1 bg-orange40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-yellow100" />
+        <div class="w-3 h-3 m-1 bg-yellow60" />
+        <div class="w-3 h-3 m-1 bg-yellow40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-green100" />
+        <div class="w-3 h-3 m-1 bg-green60" />
+        <div class="w-3 h-3 m-1 bg-green40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-blue100" />
+        <div class="w-3 h-3 m-1 bg-blue60" />
+        <div class="w-3 h-3 m-1 bg-blue40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-purple100" />
+        <div class="w-3 h-3 m-1 bg-purple60" />
+        <div class="w-3 h-3 m-1 bg-purple40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-pink100" />
+        <div class="w-3 h-3 m-1 bg-pink60" />
+        <div class="w-3 h-3 m-1 bg-pink40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-darkblue100" />
+        <div class="w-3 h-3 m-1 bg-darkblue60" />
+        <div class="w-3 h-3 m-1 bg-darkblue40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-black100" />
+        <div class="w-3 h-3 m-1 bg-black90" />
+        <div class="w-3 h-3 m-1 bg-black80" />
+        <div class="w-3 h-3 m-1 bg-black70" />
+        <div class="w-3 h-3 m-1 bg-black60" />
+        <div class="w-3 h-3 m-1 bg-black50" />
+        <div class="w-3 h-3 m-1 bg-black20" />
+        <div class="w-3 h-3 m-1 bg-black10" />
+      </div>
+      <div class="flex bg-black10">
+        <div class="w-3 h-3 m-1 bg-white100" />
+        <div class="w-3 h-3 m-1 bg-white90" />
+        <div class="w-3 h-3 m-1 bg-white80" />
+        <div class="w-3 h-3 m-1 bg-white70" />
+        <div class="w-3 h-3 m-1 bg-white60" />
+        <div class="w-3 h-3 m-1 bg-white50" />
+      </div>
+    </div>
+  </div>
+</template>
