8f804649a2592a4ba26eecb6d87171ca0e34bf53
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-19 13:51:24 +0200

    Feature: Started working on taskbar component

---
+++ frontend/components/layout/Taskbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="justify-center flex my-8">
+    <div class="bg-white100 px-[106px] py-3 flex justify-center rounded-full max-w-[1030px]">
+      <router-link :to="links[0].to" class="px-2" :class="{ 'active-link': isActive(links[0].to) }">
+        <UiTypography type="p" size="paragraph-small" :class="{ 'font-bold': isActive(links[0].to) }">
+          Home
+        </UiTypography>
+        <div v-if="isActive(links[0].to)" class="bg-darkblue100 h-0.5 rounded-full" />
+        <!-- <div v-else class="bg-darkblue100 h-0.5 rounded-full w-0 hover:w-full hover:transition-transform" /> -->
+      </router-link>
+
+      <router-link :to="links[1].to" class="px-2" :class="{ 'active-link': isActive(links[1].to) }">
+        <UiTypography type="p" size="paragraph-small" :class="{ 'font-bold': isActive(links[1].to) }">
+          Deliverables
+        </UiTypography>
+        <div v-if="isActive(links[1].to)" class="bg-darkblue100 h-0.5 rounded-full" />
+      </router-link>
+
+      <router-link :to="links[2].to" class="px-2" :class="{ 'active-link': isActive(links[2].to) }">
+        <UiTypography type="p" size="paragraph-small" :class="{ 'font-bold': isActive(links[2].to) }">
+          Projecten
+        </UiTypography>
+        <div v-if="isActive(links[2].to)" class="bg-darkblue100 h-0.5 rounded-full" />
+      </router-link>
+
+      <router-link :to="links[3].to" class="px-2" :class="{ 'active-link': isActive(links[3].to) }">
+        <UiTypography type="p" size="paragraph-small" :class="{ 'font-bold': isActive(links[3].to) }">
+          Leeruitkomsten
+        </UiTypography>
+        <div v-if="isActive(links[3].to)" class="bg-darkblue100 h-0.5 rounded-full" />
+      </router-link>
+
+      <router-link :to="links[4].to" class="px-2" :class="{ 'active-link': isActive(links[4].to) }">
+        <UiTypography type="p" size="paragraph-small" :class="{ 'font-bold': isActive(links[4].to) }">
+          Leeswijzer
+        </UiTypography>
+        <div v-if="isActive(links[4].to)" class="bg-darkblue100 h-0.5 rounded-full" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    links () {
+      return [
+        { text: 'Home', to: '/' },
+        { text: 'Deliverables', to: '/deliverables' },
+        { text: 'Projecten', to: '/projecten' },
+        { text: 'Leeruitkomsten', to: '/leeruitkomsten' },
+        { text: 'Leeswijzer', to: '/leeswijzer' }
+      ]
+    }
+  },
+  methods: {
+    isActive (route) {
+      return this.$route.path === route
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active-link {
+  font-weight: bold;
+  color: #0000FF; /* Blauwe kleur voor de actieve link */
+}
+</style>
--- frontend/pages/playground/mette.vue
+++ frontend/pages/playground/mette.vue
@@ -1,95 +1,94 @@
 <template>
   <UiContainer>
     <div class="h-full overflow-x-auto">
-      <div class="bg-white100 my-2">
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-red100" />
-          <div class="w-3 h-3 m-1 bg-red60" />
-          <div class="w-3 h-3 m-1 bg-red40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-orange100" />
-          <div class="w-3 h-3 m-1 bg-orange60" />
-          <div class="w-3 h-3 m-1 bg-orange40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-yellow100" />
-          <div class="w-3 h-3 m-1 bg-yellow60" />
-          <div class="w-3 h-3 m-1 bg-yellow40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-green100" />
-          <div class="w-3 h-3 m-1 bg-green60" />
-          <div class="w-3 h-3 m-1 bg-green40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-blue100" />
-          <div class="w-3 h-3 m-1 bg-blue60" />
-          <div class="w-3 h-3 m-1 bg-blue40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-purple100" />
-          <div class="w-3 h-3 m-1 bg-purple60" />
-          <div class="w-3 h-3 m-1 bg-purple40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-pink100" />
-          <div class="w-3 h-3 m-1 bg-pink60" />
-          <div class="w-3 h-3 m-1 bg-pink40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-darkblue100" />
-          <div class="w-3 h-3 m-1 bg-darkblue60" />
-          <div class="w-3 h-3 m-1 bg-darkblue40" />
-        </div>
-        <div class="flex">
-          <div class="w-3 h-3 m-1 bg-black100" />
-          <div class="w-3 h-3 m-1 bg-black90" />
-          <div class="w-3 h-3 m-1 bg-black80" />
-          <div class="w-3 h-3 m-1 bg-black70" />
-          <div class="w-3 h-3 m-1 bg-black60" />
-          <div class="w-3 h-3 m-1 bg-black50" />
-          <div class="w-3 h-3 m-1 bg-black20" />
-          <div class="w-3 h-3 m-1 bg-black10" />
-        </div>
-        <div class="flex bg-black10">
-          <div class="w-3 h-3 m-1 bg-white100" />
-          <div class="w-3 h-3 m-1 bg-white90" />
-          <div class="w-3 h-3 m-1 bg-white80" />
-          <div class="w-3 h-3 m-1 bg-white70" />
-          <div class="w-3 h-3 m-1 bg-white60" />
-          <div class="w-3 h-3 m-1 bg-white50" />
-        </div>
+      <LayoutTaskbar />
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-red100" />
+        <div class="w-3 h-3 m-1 bg-red60" />
+        <div class="w-3 h-3 m-1 bg-red40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-orange100" />
+        <div class="w-3 h-3 m-1 bg-orange60" />
+        <div class="w-3 h-3 m-1 bg-orange40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-yellow100" />
+        <div class="w-3 h-3 m-1 bg-yellow60" />
+        <div class="w-3 h-3 m-1 bg-yellow40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-green100" />
+        <div class="w-3 h-3 m-1 bg-green60" />
+        <div class="w-3 h-3 m-1 bg-green40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-blue100" />
+        <div class="w-3 h-3 m-1 bg-blue60" />
+        <div class="w-3 h-3 m-1 bg-blue40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-purple100" />
+        <div class="w-3 h-3 m-1 bg-purple60" />
+        <div class="w-3 h-3 m-1 bg-purple40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-pink100" />
+        <div class="w-3 h-3 m-1 bg-pink60" />
+        <div class="w-3 h-3 m-1 bg-pink40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-darkblue100" />
+        <div class="w-3 h-3 m-1 bg-darkblue60" />
+        <div class="w-3 h-3 m-1 bg-darkblue40" />
+      </div>
+      <div class="flex">
+        <div class="w-3 h-3 m-1 bg-black100" />
+        <div class="w-3 h-3 m-1 bg-black90" />
+        <div class="w-3 h-3 m-1 bg-black80" />
+        <div class="w-3 h-3 m-1 bg-black70" />
+        <div class="w-3 h-3 m-1 bg-black60" />
+        <div class="w-3 h-3 m-1 bg-black50" />
+        <div class="w-3 h-3 m-1 bg-black20" />
+        <div class="w-3 h-3 m-1 bg-black10" />
+      </div>
+      <div class="flex bg-black10">
+        <div class="w-3 h-3 m-1 bg-white100" />
+        <div class="w-3 h-3 m-1 bg-white90" />
+        <div class="w-3 h-3 m-1 bg-white80" />
+        <div class="w-3 h-3 m-1 bg-white70" />
+        <div class="w-3 h-3 m-1 bg-white60" />
+        <div class="w-3 h-3 m-1 bg-white50" />
+      </div>
 
-        <div class="my-2">
-          <UiTypography type="h1" size="heading1">
-            Heading1
-          </UiTypography>
-          <UiTypography type="h2" size="heading2">
-            Heading2
-          </UiTypography>
-          <UiTypography type="h3" size="heading3">
-            Heading3
-          </UiTypography>
-          <UiTypography type="h4" size="heading4">
-            Heading4
-          </UiTypography>
-          <UiTypography type="p">
-            Paragraph default
-          </UiTypography>
-          <UiTypography type="p" size="paragraph-small">
-            Paragraph small
-          </UiTypography>
-          <UiTypography type="p" size="paragraph-extra-small">
-            Paragraph extra small
-          </UiTypography>
-          <UiTypography type="p" size="button-big">
-            Button Big
-          </UiTypography>
-          <UiTypography type="p" size="button-regular">
-            Button Regular
-          </UiTypography>
-        </div>
+      <div class="my-2">
+        <UiTypography type="h1" size="heading1">
+          Heading1
+        </UiTypography>
+        <UiTypography type="h2" size="heading2">
+          Heading2
+        </UiTypography>
+        <UiTypography type="h3" size="heading3">
+          Heading3
+        </UiTypography>
+        <UiTypography type="h4" size="heading4">
+          Heading4
+        </UiTypography>
+        <UiTypography type="p">
+          Paragraph default
+        </UiTypography>
+        <UiTypography type="p" size="paragraph-small">
+          Paragraph small
+        </UiTypography>
+        <UiTypography type="p" size="paragraph-extra-small">
+          Paragraph extra small
+        </UiTypography>
+        <UiTypography type="p" size="button-big">
+          Button Big
+        </UiTypography>
+        <UiTypography type="p" size="button-regular">
+          Button Regular
+        </UiTypography>
       </div>
     </div>
   </UiContainer>
