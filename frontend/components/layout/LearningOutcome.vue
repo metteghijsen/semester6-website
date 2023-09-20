f9a42dd364a74dd292fe78eece520806068b7c8a
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-20 12:49:56 +0200

    Feature: Added learningOutcome component

---
+++ frontend/components/layout/LearningOutcome.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white100 max-w-[712px] rounded-3xl mb-2 mx-1">
+    <div class="pl-7">
+      <UiTypography type="h4" size="heading4" :color="loNameColor" class="pt-8">
+        {{ loName }}
+      </UiTypography>
+      <UiTypography type="p" class="opacity-50 pt-2 pb-9 pr-4 max-w-[615px]">
+        {{ loDescription }}
+      </UiTypography>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'LearningOutcome',
+  props: {
+    loName: {
+      type: String,
+      required: true
+    },
+    loDescription: {
+      type: String,
+      required: true
+    },
+    loNameColor: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
--- frontend/pages/playground/mette.vue
+++ frontend/pages/playground/mette.vue
@@ -136,5 +136,18 @@
         />
       </div>
     </div>
+
+    <div class="flex flex-row">
+      <div class="flex flex-col">
+        <LayoutLearningOutcome lo-name-color="text-red100" lo-name="1. User Interaction (Analysis & Advice)" lo-description="You analyse the user, the user interaction and the user experience, also taking state of the art interactive technologies into account. You select a suitable design process to be able to advise on UX interventions based on a validated UX design." />
+        <LayoutLearningOutcome lo-name-color="text-yellow100" lo-name="3. Software design and realisation" lo-description="You create & design software with existing components or libraries using predetermined quality criteria and version control." />
+        <LayoutLearningOutcome lo-name-color="text-blue100" lo-name="5. Investigative problem solving" lo-description="You formulate sub-questions pertaining to the primary question and answer these using relevant research methods. You use the conclusions of the sub-questions to justify (design) choices." />
+      </div>
+
+      <div class="flex flex-col">
+        <LayoutLearningOutcome lo-name-color="text-orange100" lo-name="2. User Interaction (Execution & Validation)" lo-description="You execute and evaluate the user experience of an interactive product. You document the development process for the stakeholders." />
+        <LayoutLearningOutcome lo-name-color="text-green100" lo-name="4. Future-oriented organisation" lo-description="You carry out a problem analysis and on that basis, you determine the definitive problem and elaborate on this in a project plan." />
+      </div>
+    </div>
   </UiContainer>
 </template>
