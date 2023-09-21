d01b3f5e64fab9ab3821c0201cf8281ecff00bd4
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-21 16:25:28 +0200

    Feature: Added Logo component

---
+++ frontend/components/ui/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center items-center">
+    <NuxtLink :to="to">
+      <NuxtImg :src="`/img/logos/${imageName}.png`" :alt="imageName" class="grayscale hover:grayscale-0 transition-all ease-in-out" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'Logo',
+  props: {
+    imageName: {
+      type: String,
+      required: true
+    },
+    to: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
