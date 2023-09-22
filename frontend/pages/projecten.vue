ee85c00c11ed169fa2815a1c7f058b3700b7b9ab
I481695 <mette.ghijsen@student.fontys.nl>
2023-09-22 16:13:50 +0200

    Feature: Finished project page

--- frontend/pages/projecten.vue
+++ frontend/pages/projecten.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <UiContainer>
-      <div class="h-full overflow-x-auto">
+      <div class="h-full overflow-x-hidden">
         <LayoutTaskbar />
+        <UiProject
+          image-left
+          image-name="personalproject"
+          project-name="Individueel Project"
+          project-description="
+          Dit document dient als een uitgebreid verslag van de verschillende stadia die ik heb doorlopen bij het samenstellen van mijn portfolio. Hierin wordt dieper ingegaan op de specifieke stappen en overwegingen die een rol speelden tijdens het onderzoeks-, ontwerp- en realisatieproces van mijn portfolio."
+        />
       </div>
     </UiContainer>
     <SectionsFooter />
