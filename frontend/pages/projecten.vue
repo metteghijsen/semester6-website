925af402c061d5db0a6d64e4ff70fd9a138e207d
I481695 <mette.ghijsen@student.fontys.nl>
2023-09-22 11:47:04 +0200

    Feature: Added Projects page

---
+++ frontend/pages/projecten.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <UiContainer>
+      <div class="h-full overflow-x-auto">
+        <LayoutTaskbar />
+      </div>
+    </UiContainer>
+    <SectionsFooter />
+  </div>
+</template>
