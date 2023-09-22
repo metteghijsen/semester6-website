ee0489766bfbc8d6a0c61ac98c6a1deab3894480
I481695 <mette.ghijsen@student.fontys.nl>
2023-09-22 16:15:59 +0200

    Feature: Started working on projectplan documentation

---
+++ frontend/pages/deliverable/projectplan.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <UiContainer>
+      <div class="h-full overflow-x-hidden">
+        <LayoutTaskbar />
+        <UiTextbox add-header header="Personal Project Plan" title="Inleiding" add-button to="documents/projectplandocumentation.pdf">
+          In dit document beschrijf ik mijn persoonlijke projectplan, en beschrijf ik het proces dat ik heb doorlopen bij het samenstellen van dit plan.
+        </UiTextbox>
+        <UiTextbox
+          add-image
+          image-name="cmdmethods"
+          title="Inzichten & Bevindingen"
+        >
+          Het belangrijkste doel van mijn persoonlijk projectplan is om mijn portfolio-websiteproject voor de komende drie weken te sturen en goed te plannen. Ondanks mijn natuurlijke neiging om direct aan het ontwerp te beginnen, realiseer ik me dat dit semester een onderzoekende aanpak vereist. Daarom heb ik onderzoeksvragen en -methoden zorgvuldig geformuleerd met behulp van het CMD Method Pack.
+          <br><br>Tijdens het proces heb ik twee keer feedback gekregen van Constanze, wat mijn projectaanpak heeft versterkt. Ze benadrukte het belang van het demonstreren van de kwaliteit van mijn werk in de portfolio en moedigde diepgaander onderzoek aan. Ze suggereerde ook bibliografisch onderzoek als aanvullende methode. Daarnaast heeft ze waardevolle feedback gegeven over een grafiek die ik had gemaakt, waarin ik mijn projectstappen beschreef.
+        </UiTextbox>
+        <UiTextbox title="Conclusie">
+          Het opstellen van mijn Personal Project Plan was essentieel om de koers van mijn project te bepalen en een duidelijk plan te creëren voor de komende drie weken. Door de waardevolle feedback van Constanze kreeg ik een beter idee op welke elementen ik me het beste kon focussen, vooral met betrekking tot het aantonen van de kwaliteit van mijn werk in mijn portfolio. Kortom, mijn Personal Project Plan voelt als een professionele leidraad die mij helpt om een goed ontworpen en gebruiksvriendelijk portfolio samen te stellen.
+        </UiTextbox>
+        <UiTextbox title="Leeruitkomsten">
+          •	<b> Learning outcome 4: Future-oriented organisation </b> <br>
+          You carry out a problem analysis and on that basis, you determine the definitive problem and elaborate on this in a project plan. <br><br>
+          •	<b> Learning outcome 5: Investigative problem solving </b> <br>
+          You formulate sub-questions pertaining to the primary question and answer these using relevant research methods. You use the conclusions of the sub-questions to justify (design) choices. <br><br>
+          •	<b> Learning outcome 7: Goal-oriented interaction </b> <br>
+          You communicate with different stakeholders and team members about the ICT assignment, taking into account an international context.
+        </UiTextbox>
+      </div>
+    </UiContainer>
+    <SectionsFooter />
+  </div>
+</template>
