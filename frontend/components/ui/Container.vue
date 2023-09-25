5ce2128aa31b10ee5e7b66ccfac8588c6836d838
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-25 12:45:40 +0200

    Fix: Added zoom to make elements smaller

--- frontend/components/ui/Container.vue
+++ frontend/components/ui/Container.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="bg-black10 min-h-screen">
+  <div class="bg-black10 min-h-screen zoom-80">
     <div class="max-w-[1460px] mx-auto px-3">
       <slot />
     </div>
   </div>
 </template>
+
+<style scoped>
+.zoom-80 {
+  zoom: 80%;
+}
+</style>
