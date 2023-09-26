2717f6e072a9e063521efc492dfcfd153a09ff8d
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-26 19:02:44 +0200

    Feature: Edited component to be used elsewhere

--- frontend/components/ui/LearningOutcome.vue
+++ frontend/components/ui/LearningOutcome.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-white100 max-w-[712px] rounded-3xl mb-2 mx-1">
+  <div class="bg-white100 w-full rounded-3xl my-2">
     <div class="pl-7">
       <UiTypography type="h4" size="heading4" :color="loNameColor" class="pt-8">
         {{ loName }}
       </UiTypography>
-      <UiTypography type="p" class="opacity-50 pt-2 pb-9 pr-4 max-w-[615px]">
+      <UiTypography type="p" class="opacity-50 pt-2 pb-9 pr-4">
         {{ loDescription }}
       </UiTypography>
     </div>
