0db006fcd370730b33655e1e27c605b37e7af223
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-09-26 18:50:10 +0200

    Feature: Created LearningOutcomeBullet component

---
+++ frontend/components/ui/LearningOutcomeBullet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="rounded-full h-3 flex items-center w-max px-2 my-2 mr-1 cursor-default" :class="'bg-' + bgColor" :title="title">
+      <div class="rounded-full mr-1 w-2 h-2 my-0.5" :class="'bg-' + color" />
+      <UiTypography type="p" size="paragraph-extra-small">
+        {{ label }}
+      </UiTypography>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'LearningOutcomeBullet',
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    bgColor: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
