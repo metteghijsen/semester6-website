e4d318438096653881b1a9fac4e96bbbd07d2683
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-10-03 19:01:32 +0200

    Feature: Added call to action for group project

--- frontend/pages/projecten.vue
+++ frontend/pages/projecten.vue
@@ -10,6 +10,12 @@
           project-description="
           Dit document dient als een uitgebreid verslag van de verschillende stadia die ik heb doorlopen bij het samenstellen van mijn portfolio. Hierin wordt dieper ingegaan op de specifieke stappen en overwegingen die een rol speelden tijdens het onderzoeks-, ontwerp- en realisatieproces van mijn portfolio."
         />
+        <!-- <UiProject
+          image-name="personalproject"
+          project-name="Grip on Sound"
+          project-description="
+          Dit document dient als een uitgebreid verslag van de verschillende stadia die ik heb doorlopen bij het samenstellen van mijn portfolio. Hierin wordt dieper ingegaan op de specifieke stappen en overwegingen die een rol speelden tijdens het onderzoeks-, ontwerp- en realisatieproces van mijn portfolio."
+        /> -->
       </div>
     </UiContainer>
     <SectionsFooter />
