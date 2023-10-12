bd433320e5dd76c2cc347f8e7130471d88b67770
I481695 <mette.ghijsen@student.fontys.nl>
2023-10-12 15:42:57 +0200

    Feature: Made footer logo's smaller

--- frontend/components/sections/Footer.vue
+++ frontend/components/sections/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-black10">
-    <div class="w-full bg-white100 grid grid-cols-4 gap-9 px-[340px] py-[120px]" data-aos="fade-up" data-aos-delay="50">
+    <div class="w-full bg-white100 grid grid-cols-4 gap-2 px-[450px] py-[120px]" data-aos="fade-up" data-aos-delay="50">
       <UiLogo image-name="fontys" to="https://www.fontys.nl/Home.htm" />
       <UiLogo image-name="kea" to="https://kea.dk/en/" />
       <UiLogo image-name="livewall" to="https://www.livewall.nl/" />
--- frontend/components/ui/Logo.vue
+++ frontend/components/ui/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center">
     <NuxtLink :to="to" target="_blank">
-      <NuxtImg :src="`/img/logos/${imageName}.png`" :alt="imageName" class="grayscale hover:grayscale-0 transition-all ease-in-out" />
+      <NuxtImg :src="`/img/logos/${imageName}.png`" :alt="imageName" class="grayscale hover:grayscale-0 transition-all duration-300 ease-in-out max-w-[175px]" />
     </NuxtLink>
   </div>
 </template>
