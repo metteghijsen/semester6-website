2f883eba73966c2208cf1fe65b789df6239132a0
I481695 <mette.ghijsen@student.fontys.nl>
2023-10-13 15:24:24 +0200

    Feature: Added accordeon items and removed duplicated code

--- frontend/pages/leeswijzer.vue
+++ frontend/pages/leeswijzer.vue
@@ -11,263 +11,35 @@
             title="leeswijzer"
             class="rounded-3xl"
           />
-          <div class="ml-4 w-[600px] bg-white100 rounded-3xl">
+          <div class="ml-4 w-[600px] h-max bg-white100 rounded-3xl">
             <div class="m-4">
-              <UiTypography type="h3" size="heading3" class="mb-3">
-                Burden of Proof
-              </UiTypography>
+              <!-- Loop over de leeruitkomsten -->
+              <div v-for="(outcome, index) in outcomes" :key="index">
+                <UiTypography :type="'h4'" :size="'heading4'" :color="outcome.color" class="mt-5 flex flex-row">
+                  {{ outcome.title }}
+                  <NuxtImg
+                    v-if="outcome.show === false"
+                    :src="outcome.iconFalse"
+                    class="w-3 cursor-pointer"
+                    @click="toggleLearningOutcome(index)"
+                  />
 
-              <!-- Leeruitkomst 1 -->
-              <div>
-                <UiTypography type="h4" size="heading4" color="text-red100">
-                  UI (Analysis & Advice)
+                  <NuxtImg
+                    v-if="outcome.show === true"
+                    :src="outcome.iconTrue"
+                    class="w-3 cursor-pointer"
+                    @click="toggleLearningOutcome(index)"
+                  />
                 </UiTypography>
 
-                <NuxtLink to="/deliverable/literaturestudy">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Trendanalyse
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/colorresearch">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Color Research
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/immersion">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Immersion
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/interview">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Interview
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/wireframes">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Wireframing
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/usertesting">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Usertesting
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/abtesting">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • A/B Testing
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/c4model">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • C4 model
-                  </UiTypography>
-                </NuxtLink>
-              </div>
-
-              <!-- Leeruitkomst 2 -->
-              <div>
-                <UiTypography type="h4" size="heading4" color="text-orange100" class="mt-5">
-                  UI (Execution & Validation)
-                </UiTypography>
-
-                <NuxtLink to="/deliverable/wireframes">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Wireframing
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/usertesting">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Usertesting
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/abtesting">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • A/B Testing
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/developmentwebsite">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Development Website
-                  </UiTypography>
-                </NuxtLink>
-              </div>
-
-              <!-- Leeruitkomst 3 -->
-              <div>
-                <UiTypography type="h4" size="heading4" color="text-yellow100" class="mt-5">
-                  Software Design & Realisation
-                </UiTypography>
-
-                <NuxtLink to="/deliverable/developmentwebsite">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Development Website
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/c4model">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • C4 model
-                  </UiTypography>
-                </NuxtLink>
-              </div>
-
-              <!-- Leeruitkomst 4 -->
-              <div>
-                <UiTypography type="h4" size="heading4" color="text-green100" class="mt-5">
-                  Future Oriented Organisation
-                </UiTypography>
-
-                <NuxtLink to="/deliverable/projectplan">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Projectplan
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/leeswijzer">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Leeswijzer
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/brainstorming">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Brainstorming
-                  </UiTypography>
-                </NuxtLink>
-              </div>
-
-              <!-- Leeruitkomst 5 -->
-              <div>
-                <UiTypography type="h4" size="heading4" color="text-blue100" class="mt-5">
-                  Investigative Problem Solving
-                </UiTypography>
-
-                <NuxtLink to="/deliverable/projectplan">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Projectplan
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/literaturestudy">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Trendanalyse
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/colorresearch">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Color Research
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/immersion">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Immersion
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/interview">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Interview
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/usertesting">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Usertesting
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/abtesting">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • A/B Testing
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/c4model">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • C4 model
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/leeswijzer">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Leeswijzer
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/brainstorming">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Brainstorming
-                  </UiTypography>
-                </NuxtLink>
-              </div>
-
-              <!-- Leeruitkomst 6 -->
-              <div>
-                <UiTypography type="h4" size="heading4" color="text-purple100" class="mt-5">
-                  Personal Leadership
-                </UiTypography>
-
-                <NuxtLink to="/deliverable/developmentwebsite">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Development Website
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/postersession">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Poster Session
-                  </UiTypography>
-                </NuxtLink>
-              </div>
-
-              <!-- Leeruitkomst 7 -->
-              <div>
-                <UiTypography type="h4" size="heading4" color="text-pink100" class="mt-5">
-                  Goal-oriented Interaction
-                </UiTypography>
-
-                <NuxtLink to="/deliverable/projectplan">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Projectplan
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/c4model">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • C4 model
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/leeswijzer">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Leeswijzer
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/postersession">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Poster Session
-                  </UiTypography>
-                </NuxtLink>
-
-                <NuxtLink to="/deliverable/postersession">
-                  <UiTypography type="p" class="ml-3 my-0.5 hover:opacity-50 transition-all">
-                    • Brainstorming
-                  </UiTypography>
-                </NuxtLink>
+                <div v-if="outcome.show === true">
+                  <!-- Loop over de links voor deze leeruitkomst -->
+                  <NuxtLink v-for="(link, linkIndex) in outcome.links" :key="linkIndex" :to="link.to">
+                    <UiTypography :type="'p'" class="ml-3 my-0.5 hover:opacity-50 transition-all">
+                      {{ link.text }}
+                    </UiTypography>
+                  </NuxtLink>
+                </div>
               </div>
             </div>
           </div>
@@ -277,3 +49,121 @@
     <SectionsFooter />
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      outcomes: [
+        {
+          title: 'UI (Analysis & Advice)',
+          color: 'text-red100',
+          iconTrue: 'img/chevron-down.png',
+          iconFalse: 'img/chevron-right.png',
+          links: [
+            { to: '/deliverable/literaturestudy', text: '• Trendanalyse' },
+            { to: '/deliverable/colorresearch', text: '• Color Research' },
+            { to: '/deliverable/immersion', text: '• Immersion' },
+            { to: '/deliverable/interview', text: '• Interview' },
+            { to: '/deliverable/wireframes', text: '• Wireframing' },
+            { to: '/deliverable/usertesting', text: '• Usertesting' },
+            { to: '/deliverable/abtesting', text: '• A/B Testing' },
+            { to: '/deliverable/c4model', text: '• C4 model' }
+          ],
+          show: true
+        },
+        {
+          title: 'UI (Execution & Validation)',
+          color: 'text-orange100',
+          iconTrue: 'img/chevron-down.png',
+          iconFalse: 'img/chevron-right.png',
+          links: [
+            { to: '/deliverable/wireframes', text: '• Wireframing' },
+            { to: '/deliverable/usertesting', text: '• Usertesting' },
+            { to: '/deliverable/abtesting', text: '• A/B Testing' },
+            { to: '/deliverable/developmentwebsite', text: '• Development Website' }
+          ],
+          show: false
+        },
+        {
+          title: 'Software Design & Realisation',
+          color: 'text-yellow100',
+          iconTrue: 'img/chevron-down.png',
+          iconFalse: 'img/chevron-right.png',
+          links: [
+            { to: '/deliverable/developmentwebsite', text: '• Development Website' },
+            { to: '/deliverable/c4model', text: '• C4 model' },
+            { to: '/deliverable/pipeline', text: '• Pipeline' }
+          ],
+          show: false
+        },
+        {
+          title: 'Future Oriented Organisation',
+          color: 'text-green100',
+          iconTrue: 'img/chevron-down.png',
+          iconFalse: 'img/chevron-right.png',
+          links: [
+            { to: '/deliverable/projectplan', text: '• Projectplan' },
+            { to: '/deliverable/leeswijzer', text: '• Leeswijzer' },
+            { to: '/deliverable/brainstorming', text: '• Brainstorming' },
+            { to: '/deliverable/projectplangroup', text: '• Projectplan Group' }
+          ],
+          show: false
+        },
+        {
+          title: 'Investigative Problem Solving',
+          color: 'text-blue100',
+          iconTrue: 'img/chevron-down.png',
+          iconFalse: 'img/chevron-right.png',
+          links: [
+            { to: '/deliverable/projectplan', text: '• Projectplan' },
+            { to: '/deliverable/literaturestudy', text: '• Trendanalyse' },
+            { to: '/deliverable/colorresearch', text: '• Color Research' },
+            { to: '/deliverable/immersion', text: '• Immersion' },
+            { to: '/deliverable/interview', text: '• Interview' },
+            { to: '/deliverable/usertesting', text: '• Usertesting' },
+            { to: '/deliverable/abtesting', text: '• A/B Testing' },
+            { to: '/deliverable/c4model', text: '• C4 model' },
+            { to: '/deliverable/pipeline', text: '• Pipeline' },
+            { to: '/deliverable/leeswijzer', text: '• Leeswijzer' },
+            { to: '/deliverable/brainstorming', text: '• Brainstorming' },
+            { to: '/deliverable/projectplangroup', text: '• Projectplan Group' }
+          ],
+          show: false
+        },
+        {
+          title: 'Personal Leadership',
+          color: 'text-purple100',
+          iconTrue: 'img/chevron-down.png',
+          iconFalse: 'img/chevron-right.png',
+          links: [
+            { to: '/deliverable/developmentwebsite', text: '• Development Website' },
+            { to: '/deliverable/postersession', text: '• Poster Session' }
+          ],
+          show: false
+        },
+        {
+          title: 'Goal-oriented Interaction',
+          color: 'text-pink100',
+          iconTrue: 'img/chevron-down.png',
+          iconFalse: 'img/chevron-right.png',
+          links: [
+            { to: '/deliverable/projectplan', text: '• Projectplan' },
+            { to: '/deliverable/c4model', text: '• C4 model' },
+            { to: '/deliverable/leeswijzer', text: '• Leeswijzer' },
+            { to: '/deliverable/postersession', text: '• Poster Session' },
+            { to: '/deliverable/brainstorming', text: '• Brainstorming' },
+            { to: '/deliverable/projectplangroup', text: '• Projectplan Group' }
+          ],
+          show: false
+        }
+      ]
+    }
+  },
+  methods: {
+    toggleLearningOutcome (index) {
+      this.outcomes[index].show = !this.outcomes[index].show
+    }
+  }
+}
+</script>
