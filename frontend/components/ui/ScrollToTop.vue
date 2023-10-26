53c28bc7677e56344a9033eb0d6cfa21b0828f58
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-10-26 12:59:11 +0200

    Feature: Added scroll to top button

---
+++ frontend/components/ui/ScrollToTop.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    v-if="showButton"
+    class="fixed bottom-4 right-4 bg-darkblue100 text-white px-2 py-2 rounded-full transition-all"
+    @click="scrollToTop"
+  >
+    <NuxtImg src="/img/arrow-up.png" alt="ScrollToTop" />
+  </button>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+
+const showButton = ref(false)
+
+const handleScroll = () => {
+  showButton.value = window.scrollY > 300
+}
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+</script>
