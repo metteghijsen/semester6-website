6558ced07a89f7638f3bcc01b0dd47727c2065b3
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-10-31 18:07:24 +0100

    Feature: Added custom error page

---
+++ frontend/pages/error.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    <div class="h-screen flex justify-center flex-col items-center">
+      <UiTypography type="h1" size="heading1">
+        404
+      </UiTypography>
+      <UiTypography type="h3" size="heading3">
+        Page Not Found
+      </UiTypography>
+      <UiTypography type="p" size="paragraph-small">
+        Sorry, the page you are looking for might be in another castle.
+      </UiTypography>
+    </div>
+  </div>
+</template>
