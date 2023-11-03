f3e8c5fbce5a611bb7205ecfa834eb6b701b0db1
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-11-03 14:55:17 +0100

    Feature: Added documentation for architecture museum deliverable

---
+++ frontend/pages/deliverable/architecturemuseum.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <UiContainer>
+      <div class="h-full ">
+        <LayoutTaskbar />
+        <UiTextbox
+          add-header
+          header="Danish Architecture Center"
+          title="Inleiding"
+          lo7
+        >
+          Tijdens de herfstvakantie heb ik een reis gemaakt naar Kopenhagen. Hier heb ik het architectuur museum (Danish Architecture Centre) bekeken. Dit museum, gelegen in het hart van de stad, biedt een boeiende kijk op de rijke geschiedenis van Deense architectuur en de evolutie ervan door de jaren heen. In dit stuk documentatie bespreek ik de interessante dingen die ik heb gezien.
+        </UiTextbox>
+        <UiTextbox
+          title="Aankomst en Exterieur"
+          add-image
+          image-name="blox"
+        >
+          De buitenkant van het gebouw zag er al prachtig uit. Bij aankomst viel meteen de moderne architectuur van het museum op, die overigens in heel Kopenhagen te zien is. Het gebouw zelf is een meesterwerk, een combinatie van strakke lijnen en innovatieve ontwerpelementen die een voorproefje geven van wat er binnen te wachten staat. De entree was uitnodigend en beloofde een ontdekkingstocht door de fascinerende wereld van de Deense architectuur. In het gebouw was zelfs een glijbaan te vinden, die je van buiten al door de ramen kon zien.
+        </UiTextbox>
+        <UiTextbox
+          title="Interieur"
+          add-image
+          image-name="superkilenbolgenbasecamp"
+        >
+          Een opvallende tentoonstelling in het museum was gewijd aan Superkilen, het bekende gestreepte park. Met een oppervlakte van meer dan 30.000 m2 en 99 objecten uit 59 verschillende landen, bood de tentoonstelling een uitgebreid inzicht in de diversiteit en internationaliteit van dit openbare park, dat zijn deuren opende in 2012.
+          <br><br>
+          Een ander architectonisch hoogtepunt was De Bolgen, een gebouw met opvallende golfdaken. De tentoonstelling werpte licht op de ontwerpprincipes achter dit meesterwerk en illustreerde hoe architectuur niet alleen functioneel kan zijn, maar ook esthetisch indrukwekkend.
+          <br><br>
+          De Basecamp studentenwoningen werden ook uitgelicht in het museum, met aandacht voor hun unieke ontwerp en het geïntegreerde groene dak. De tentoonstelling bood een gedetailleerde verkenning van de innovatieve aspecten van dit project en de symbiose tussen architectuur en natuur.
+          <br><br>
+          Ten slotte werd Kastrup Sobad, een zwemplek aan de kust met uitzicht op Zweden, gepresenteerd als een uniek ontworpen locatie. De tentoonstelling benadrukte niet alleen het architecturale ontwerp, maar ook de relatie tussen mens en natuur, waardoor het een bijzondere plek werd om te verkennen.
+        </UiTextbox>
+        <UiTextbox
+          title="Conclusie"
+        >
+          Al met al bood het Deense Architectuurmuseum een verrijkende ervaring, waarbij ik niet alleen de architectuur van Kopenhagen ontdekte, maar ook een dieper begrip kreeg van de creatieve processen en concepten achter deze opmerkelijke bouwwerken. Het bezoek was zowel informatief als inspirerend, en ik vertrok met een grotere waardering voor de impact van goede architectuur op de stedelijke omgeving.
+        </UiTextbox>
+        <UiTextbox title="Leeruitkomsten">
+          •	<b> Learning outcome 7: Goal-oriented interaction </b> <br>
+          You communicate with different stakeholders and team members about the ICT assignment, taking into account an international context.
+        </UiTextbox>
+      </div>
+    </UiContainer>
+    <SectionsFooter />
+  </div>
+</template>
