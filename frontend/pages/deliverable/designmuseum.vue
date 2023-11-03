fd34bda98c7e4f925e027497c4a84db93a4a1967
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-11-03 14:55:27 +0100

    Feature: Added documentation for design museum deliverable

---
+++ frontend/pages/deliverable/designmuseum.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <UiContainer>
+      <div class="h-full ">
+        <LayoutTaskbar />
+        <UiTextbox
+          add-header
+          header="Design Museum Kopenhagen"
+          title="Inleiding"
+          lo7
+        >
+          Tijdens de herfstvakantie heb ik een reis gemaakt naar Kopenhagen. Hier heb ik het Designmuseum bezocht. Gelegen in het hart van de stad, bood het museum een verfrissende kijk op de evolutie en diversiteit van Deens design, zowel historisch als hedendaags. In dit stuk documentatie bespreek ik de interessante dingen die ik heb gezien.
+        </UiTextbox>
+        <UiTextbox
+          title="Tentoonstelling over de toekomst"
+          add-image
+          image-name="future"
+        >
+          De expositie over de wereld van de toekomst bood boeiende inzichten in trends zoals remote werken, waarbij de voordelen en mogelijke toekomstige vormen van deze manier van werken werden verkend. Het aspect van artificial intelligence (AI) trok mijn aandacht, vooral het vooruitzicht van verbeterde en nauwkeurigere interacties met AI-systemen in de toekomst.
+          <br><br>
+          Een intrigerend onderdeel van de tentoonstelling behandelde stedelijk design, waarbij de vraag werd gesteld of we steden konden ontwerpen die niet alleen functioneel zijn voor mensen, maar ook een thuis bieden aan dieren en de natuur. Dit vond ik een erg interessant concept, en zeker iets wat we meer zouden kunnen meenemen bij het ontwerpen van steden. De natuur heeft vaak ook een positief effect op mensen.
+          <br><br>
+          Een van de meest boeiende thema's was het "Right to Disconnect", waarbij de noodzaak werd besproken om een wet te implementeren die individuen beschermt tegen het constante verwachtingspatroon van 24/7 bereikbaarheid. Dit is een concept waar ik me erg goed in kan vinden, omdat het benadrukt dat het cruciaal is om afstand te nemen van de digitale wereld om een evenwichtiger en bevredigender leven te leiden.
+        </UiTextbox>
+        <UiTextbox
+          title="Andere tentoonstellingen"
+          add-image
+          image-name="patronen"
+        >
+          Na deze interessante ontdekkingstocht ging ik door naar de tentoonstelling over patronen, waar ik meer te weten kwam over hoe verschillende ontwerppatronen ontstaan en gebruikt worden. Het was niet alleen leuk om naar te kijken, maar het gaf ook een kijkje in hoe design door de jaren heen is veranderd.
+          <br><br>
+          Vervolgens dook ik in de interessante geschiedenis van Deens meubelontwerp. Daar zag ik allerlei mooie en bekende Deense stoelen, en er was een video die liet zien hoe een stoel wordt ontworpen. Het was echt gaaf om te zien hoeveel vakmanschap en toewijding er nodig is om die tijdloze meubelstukken te maken.
+        </UiTextbox>
+        <UiTextbox
+          title="Conclusie"
+        >
+          Kortom, mijn bezoek aan het Designmuseum in Kopenhagen was super inspirerend en leerzaam. Het museum liet niet alleen zien hoe de toekomst van design eruit zou kunnen zien, maar bracht ook de gave geschiedenis van Deens design tot leven. Het was gewoon een dag vol creativiteit, nieuwe ideeën en een beter begrip van wat design allemaal kan betekenen.
+        </UiTextbox>
+        <UiTextbox title="Leeruitkomsten">
+          •	<b> Learning outcome 7: Goal-oriented interaction </b> <br>
+          You communicate with different stakeholders and team members about the ICT assignment, taking into account an international context.
+        </UiTextbox>
+      </div>
+    </UiContainer>
+    <SectionsFooter />
+  </div>
+</template>
