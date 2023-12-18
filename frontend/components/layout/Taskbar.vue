3a1d2eeb7afb5eba3ccc11cacefc31f9974faad8
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2023-12-18 11:20:18 +0100

    Feature: Fixed poor text placement on bigger screens, added hover to github, darktheme buttons

--- frontend/components/layout/Taskbar.vue
+++ frontend/components/layout/Taskbar.vue
@@ -1,51 +1,53 @@
 <template>
   <div>
     <div class="justify-center flex mb-[135px]">
-      <div class="bg-white100 border-b-slate-200 dark:bg-slate-800 dark:border-b-slate-600 border-b-2 py-5 flex flex-row items-center flex-wrap w-full fixed top-0 z-40">
+      <div class="bg-white100 border-b-slate-200 dark:bg-slate-800 dark:border-b-slate-600 border-b-2 py-4 flex flex-row items-center w-full fixed top-0 z-40">
         <NuxtLink to="/">
-          <UiTypography type="p" size="heading4" class="font-bold px-8 mr-[800px]">
+          <UiTypography type="p" size="heading4" class="font-bold px-8 whitespace-nowrap">
             Mette Ghijsen
           </UiTypography>
         </NuxtLink>
-        <router-link
-          v-for="(link, index) in links"
-          :key="index"
-          :to="link.to"
-          class="px-2 link"
-          :class="{ 'active-link': isActive(link.to) }"
-        >
-          <UiTypography type="p" size="paragraph-small" :class="{ 'font-bold': isActive(link.to) }">
-            {{ link.text }}
-          </UiTypography>
-          <div
-            v-if="isActive(link.to)"
-            class="bg-darkblue100 dark:bg-blue100 h-0.5 rounded-full"
-          />
-          <div
-            v-else
-            class="bg-darkblue100 dark:bg-blue100 h-0.5 rounded-full stripe"
-          />
-        </router-link>
+        <div class="flex justify-end w-full items-center">
+          <router-link
+            v-for="(link, index) in links"
+            :key="index"
+            :to="link.to"
+            class="px-2 link"
+            :class="{ 'active-link': isActive(link.to) }"
+          >
+            <UiTypography type="p" size="paragraph-small" :class="{ 'font-bold': isActive(link.to) }" class="">
+              {{ link.text }}
+            </UiTypography>
+            <div
+              v-if="isActive(link.to)"
+              class="bg-darkblue100 dark:bg-blue100 h-0.5 rounded-full"
+            />
+            <div
+              v-else
+              class="bg-darkblue100 dark:bg-blue100 h-0.5 rounded-full stripe"
+            />
+          </router-link>
+
+          <NuxtLink to="https://github.com/metteghijsen/semester6-website" class="p-1 mx-2 ml-12 hover:bg-slate-100  dark:hover:bg-slate-700 rounded-md ease-in-out transition-colors">
+            <div v-if="isDarkMode == true">
+              <Github class="h-3 w-3" color="white" />
+            </div>
+            <div v-else>
+              <Github class="h-3 w-3" />
+            </div>
+          </NuxtLink>
+
+          <button class="p-1 mr-10 dark:hover:bg-slate-700 hover:bg-slate-100 rounded-md ease-in-out transition-colors" @click="toggleDarkMode">
+            <div v-if="isDarkMode == true">
+              <Moon class="h-3 w-3" color="white" />
+            </div>
+            <div v-else>
+              <Sun class="h-3 w-3" />
+            </div>
+          </button>
+        </div>
       </div>
     </div>
-
-    <button class="z-50 fixed top-5 right-[160px]" @click="toggleDarkMode">
-      <div v-if="isDarkMode == true">
-        <Moon class="h-3 w-3" color="white" />
-      </div>
-      <div v-else>
-        <Sun class="h-3 w-3" />
-      </div>
-    </button>
-
-    <NuxtLink to="https://github.com/metteghijsen/semester6-website" class="z-50 fixed top-5 right-[205px]">
-      <div v-if="isDarkMode == true">
-        <Github class="h-3 w-3" color="white" />
-      </div>
-      <div v-else>
-        <Github class="h-3 w-3" />
-      </div>
-    </NuxtLink>
 
     <div :class="{ 'dark': isDarkMode }">
       <div class="transition-colors ease-in-out delay-150">
